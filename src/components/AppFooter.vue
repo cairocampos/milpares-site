0b3941d39b08e3c55779bdb7b0a6e2d6704e616b
Cairo Campos <cairocampos98@gmail.com>
2022-09-14 09:41:08 -0300

    corrige link whatsapp

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,5 +2,33 @@
   <footer class="footer">
     <h2>MIL PARES | UM MUNDO DE POSSIBILIDADES</h2>
     <h2>Av. Padre Arlindo Vieira, 1277 - Vila das Mêrces, São Paulo - S</h2>
+    <a href="https://api.whatsapp.com/send?phone=5533991397833">Whatsapp</a>
+    <a
+      href="https://api.whatsapp.com/send?phone=5533991397833"
+      target="_blank"
+    >Whatsapp</a>
+    <button @click="teste()">
+      Redirect
+    </button>
+    <button @click="send()">
+      Redirect
+    </button>
+    <button @click="send()">
+      Redirect2
+    </button>
   </footer>
 </template>
+
+<script lang="ts" setup>
+const teste = () => {
+  location.href = "https://api.whatsapp.com/send?phone=5533991397833"
+}
+
+const send = () => {
+  location.href = "https://api.whatsapp.com/send?phone=5533991397833"
+}
+
+const send2 = () => {
+  window.open("https://api.whatsapp.com/send?phone=5533991397833", '_blank')
+}
+</script>
